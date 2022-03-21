f00dfa19232581171e1c2e3f00bcf47f8858cb8d
Raheel Ahmed <raheelahmed017@gmail.com>
2022-03-21 15:00:04 +0500

    first commit

---
+++ vue/src/views/SurveyView.vue
@@ -0,0 +1,13 @@
+<template>
+  <PageComponent title="View or Create Survey">
+    View or Create Survey
+  </PageComponent>
+</template>
+<script setup>
+
+import PageComponent from "../components/PageComponent.vue";
+
+
+</script>
+
+<style></style>
